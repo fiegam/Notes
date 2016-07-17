1d8aab0b607db3c0aab3e167a20f49c6f44d7a69
Mirek <fiegam@gmail.com>
2016-07-17 23:05:07 +0200

    init

---
+++ Notes.WebApi/Modules/InfoModule.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Notes.WebApi.Modules
+{
+    public class InfoModule: NancyModule
+    {
+        public InfoModule()
+        {
+            Get("/", Info);
+        }
+
+        private async Task<object> Info(dynamic parameters)
+        {
+            return "Working";
+        }
+    }
+}
